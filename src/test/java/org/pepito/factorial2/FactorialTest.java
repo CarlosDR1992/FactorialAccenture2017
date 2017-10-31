04d597c0b6f6f7135ed7b092b06f29d67bb53cad
Carlos Ramos <CarlosRamos@alu-128-188.wifi.uma.es>
2017-10-31 19:41:27 +0100

    Update FactorialTest.class

--- src/test/java/org/pepito/factorial2/FactorialTest.java
+++ src/test/java/org/pepito/factorial2/FactorialTest.java
@@ -2,52 +2,54 @@ package org.pepito.factorial2;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class FactorialTest {
+	private Factorial factorial;
+
+	@Before
+	public void init() {
+		factorial = new Factorial();
+	}
 
 	@Test
-	public void test1() {
-		Factorial factorial = new Factorial();
+	public void shouldFactorialZerofReturnOne() {
 		long result = factorial.compute(0);
 		long expectedValue = 1;
-		
+
 		assertEquals(expectedValue, result);
 	}
-	
+
 	@Test
-	public void test2() {
-		Factorial factorial = new Factorial();
+	public void shouldFactorialOnefReturnOne() {
 		long result = factorial.compute(1);
 		long expectedValue = 1;
-		
+
 		assertEquals(expectedValue, result);
 	}
-	
+
 	@Test
-	public void test3() {
-		Factorial factorial = new Factorial();
+	public void shouldFactorialTwofReturnTwo() {
 		long result = factorial.compute(2);
 		long expectedValue = 2;
-		
+
 		assertEquals(expectedValue, result);
 	}
-	
+
 	@Test
-	public void test4() {
-		Factorial factorial = new Factorial();
+	public void shouldFactorialThreefReturnSix() {
 		long result = factorial.compute(3);
 		long expectedValue = 6;
-		
+
 		assertEquals(expectedValue, result);
 	}
-	
+
 	@Test
-	public void test5() {
-		Factorial factorial = new Factorial();
+	public void shouldFactorialFivefRetur120() {
 		long result = factorial.compute(5);
 		long expectedValue = 120;
-		
+
 		assertEquals(expectedValue, result);
 	}
 }
